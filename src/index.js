a1ab5f9993dc450a1f0c52f68162474a86b3f966
王宏亮 <401765060@qq.com>
2022-03-28 10:04:42 +0800

    暴露查找进程的方法

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-const { start,kill } = require('./processStarter');
+const { start,kill,find } = require('./processStarter');
 
 /* start('D:\\tool\\KWMusic\\KwMusic.exe').then((a)=>{
     console.log(a)
 }) */
 
 
-kill(3152).then(()=>{
+/* kill(3152).then(()=>{
 
 }).catch(e=>{
     console.log('e',e)
+}) */
+
+find('KwMusic.exe').then((p)=>{
+    console.log(p)
 })
